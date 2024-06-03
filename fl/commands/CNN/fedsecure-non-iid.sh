f26e6ad287f2daa4b1c68bb1c94fcfb045a1e40d
Rushclin Takam <takamrushclin@gmail.com>
2024-06-03 19:39:23 +0100

    Edit for CNN

--- fl/commands/CNN/fedsecure-non-iid.sh
+++ fl/commands/CNN/fedsecure-non-iid.sh
@@ -8,5 +8,5 @@ python3 main.py \
     --model_name TwoCNN --resize 256 --hidden_size 200 \
     --algorithm fedsecure --eval_fraction 1 --eval_type both --eval_every 1 --eval_metrics acc1 precision f1 recall \
     --K 10 --R 20 --E 1 --C 0.1 --B 5 --beta1 0 \
-    --optimizer SGD --lr 0.01 --lr_decay 0.99 --lr_decay_step 25 --criterion CrossEntropyLoss \
+    --optimizer SGD --lr 0.01 --lr_decay 0.99 --lr_decay_step 10 --criterion CrossEntropyLoss \
     --num_classes 15 --in_channels 3 
