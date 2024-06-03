cc753151f699060842e5604a01a1924db11ae543
Rushclin Takam <takamrushclin@gmail.com>
2024-06-03 19:36:57 +0100

    Edit for CNN

--- fl/process/fedavg-iid-resnet.py
+++ fl/process/fedavg-iid-resnet.py
@@ -1,9 +1,28 @@
-import os
+# import os
+# import subprocess
+
+# # Assurez-vous que le chemin vers le script est correct et que le script est exécutable
+# script_path = "./commands/ResNet/fedavg-iid.sh"
+
+# # Lancer le script en arrière-plan
+# # os.spawnl(os.P_DETACH, script_path)
+# subprocess.Popen([script_path])
+
+
+
 import subprocess
 
-# Assurez-vous que le chemin vers le script est correct et que le script est exécutable
-script_path = "./commands/ResNet/fedavg-iid.sh"
+# Informations de connexion SSH
+ssh_user = "ml_admin"
+ssh_host = "8.tcp.eu.ngrok.io"
+ssh_key_path = "C:/Users/Rushclin02/.ssh/id_rsa" 
+remote_script_path = "./commands/ResNet/fedavg-iid.sh"
 
-# Lancer le script en arrière-plan
-# os.spawnl(os.P_DETACH, script_path)
-subprocess.Popen([script_path])
+# Commande SSH pour lancer le script avec nohup
+ssh_command = f'ssh -i {ssh_key_path} {ssh_user}@{ssh_host} "nohup {remote_script_path} > /dev/null 2>&1 &"'
+
+# Lancer la commande SSH avec subprocess
+process = subprocess.Popen(ssh_command, shell=True)
+process.wait()
+
+print("Script lancé en arrière-plan sur la machine distante")
