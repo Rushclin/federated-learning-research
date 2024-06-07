247c2bf2cdb3a3ec0028d639513f3810472d9d4d
Rushclin Takam <takamrushclin@gmail.com>
2024-06-07 04:27:19 +0100

    Edit VGG

--- fl/commands/VGG/fedavg-non-iid.sh
+++ fl/commands/VGG/fedavg-non-iid.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 ## NON-IID
-py main.py \
+python3 main.py \
     --exp_name "FedAvg_PLANT_VILLAGE_VGG13BN_NON_IID" --seed 42 --device cpu \
     --dataset PLANT_VILLAGE \
     --split_type non-iid --test_size 0 \
     --model_name VGG13BN --resize 256 --hidden_size 200 \
     --algorithm fedavg --eval_fraction 1 --eval_type both --eval_every 1 --eval_metrics acc1 precision f1 recall \
-    --K 100 --R 250 --E 1 --C 0.1 --B 5--beta1 0 \
-    --optimizer SGD --lr 0.01 --lr_decay 0.999 --lr_decay_step 10 --criterion CrossEntropyLoss \
-    --num_classes 4 --in_channels 3 
+    --K 10 --R 20 --E 1 --C 0.1 --B 5--beta1 0 \
+    --optimizer SGD --lr 0.1 --lr_decay 0.999 --lr_decay_step 10 --criterion CrossEntropyLoss \
+    --num_classes 15 --in_channels 3 
 
--- fl/commands/VGG/fedprox-non-iid.sh
+++ fl/commands/VGG/fedprox-non-iid.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 ## NON-IID
-py main.py \
+python3 main.py \
     --exp_name "FedProx_PLANT_VILLAGE_VGG13BN_NON_IID" --seed 42 --device cpu \
     --dataset PLANT_VILLAGE \
     --split_type non-iid --test_size 0 \
     --model_name VGG13BN --resize 256 --hidden_size 200 \
-    --algorithm fedavg --eval_fraction 1 --eval_type both --eval_every 1 --eval_metrics acc1 precision f1 recall \
-    --K 100 --R 250 --E 1 --C 0.1 --B 5--beta1 0 \
-    --optimizer SGD --lr 0.01 --lr_decay 0.999 --lr_decay_step 10 --criterion CrossEntropyLoss \
-    --num_classes 4 --in_channels 3 \
+    --algorithm fedprox --eval_fraction 1 --eval_type both --eval_every 1 --eval_metrics acc1 precision f1 recall \
+    --K 10 --R 20 --E 1 --C 0.1 --B 5--beta1 0 \
+    --optimizer SGD --lr 0.1 --lr_decay 0.999 --lr_decay_step 10 --criterion CrossEntropyLoss \
+    --num_classes 15 --in_channels 3 \
     --use_tb --tb_host "127.0.0.1" --tb_port 6006 \
 
