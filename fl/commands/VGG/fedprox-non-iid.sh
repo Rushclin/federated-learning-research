fac09451d68dcdc93f73eeadf1627548f5daeb35
Rushclin Takam <takamrushclin@gmail.com>
2024-06-07 19:56:53 +0100

    Edit VGG

--- fl/commands/VGG/fedprox-non-iid.sh
+++ fl/commands/VGG/fedprox-non-iid.sh
@@ -7,7 +7,7 @@ python3 main.py \
     --split_type non-iid --test_size 0 \
     --model_name VGG13BN --resize 256 --hidden_size 200 \
     --algorithm fedprox --eval_fraction 1 --eval_type both --eval_every 1 --eval_metrics acc1 precision f1 recall \
-    --K 10 --R 20 --E 1 --C 0.1 --B 5--beta1 0 \
+    --K 10 --R 20 --E 1 --C 0.1 --B 5 --beta1 0 \
     --optimizer SGD --lr 0.1 --lr_decay 0.999 --lr_decay_step 10 --criterion CrossEntropyLoss \
     --num_classes 15 --in_channels 3 \
     --use_tb --tb_host "127.0.0.1" --tb_port 6006 \
